bea0e2dcc247a8ef8dff697c395f83d57abf24f5
italo <italogithub@gmail.com>
2023-03-09 06:45:37 -0300

    update

--- POO-Java/Exercicios/src/AtividadesPrimeiraUnidade/Atividade1/AppMercado.java
+++ POO-Java/Exercicios/src/AtividadesPrimeiraUnidade/Atividade1/AppMercado.java
@@ -1,30 +1,33 @@
 package AtividadesPrimeiraUnidade.Atividade1;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class AppMercado {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Mercado mercado = new Mercado();
         Mercado.Vendas vendas = new Mercado.Vendas();
-        Mercado.Produto produto = new Mercado.Produto();
+
 
         System.out.println(" ========== Zeca Barrão =========== ");
         System.out.println(" =                                = ");
         System.out.println(" Digite o nome produto: ");
-        produto.descricao = scan.nextLine();
+        String nomeProduto = scan.nextLine();
         System.out.println(" Digite o valor do produto: ");
-        produto.preco = scan.nextDouble();
+        double precoProtudo = scan.nextDouble();
         System.out.println("Digite a quantidade de produtos: ");
-
         double quantidadeProdutos = scan.nextDouble();
+        scan.nextLine(); // consumir a quebra de linha deixada pelo nextDouble()
+        System.out.println("Digite a data de vencimento: ");
+        String dataVencimento = scan.nextLine();
+
+        Mercado.Produto produto = new Mercado.Produto(precoProtudo, nomeProduto, dataVencimento);
         Mercado.Item quant = new Mercado.Item(quantidadeProdutos, produto);
 
-
-
-        System.out.println("Nome do produto: " + produto.descricao);
-        System.out.println("Valor do produto: " + produto.preco);
-        System.out.println("Total: " + quant.total());
-
+        System.out.println("Descrição do produto: " + nomeProduto);
+        System.out.println("Valor do produto: " + precoProtudo);
+        System.out.println("Data de vencimento: " + dataVencimento);
+        System.out.println("Data da compra: " + vendas.dataAtual);
+        System.out.println("Total: " + " R$ " + quant.total());
     }
 }
--- POO-Java/Exercicios/src/AtividadesPrimeiraUnidade/Atividade1/Mercado.java
+++ POO-Java/Exercicios/src/AtividadesPrimeiraUnidade/Atividade1/Mercado.java
@@ -7,52 +7,59 @@ import java.util.List;
 public class Mercado{
 
 
+    public static Mercado.Produto Produto;
+
     public static class Vendas{
-        Date dataAtual;
-        List<Mercado.Item> itens;
-        double getTotal() {
-            double total = itens.stream()
-                    .mapToDouble(item -> item.total())
-                    .reduce(0, (acc, preco) -> acc + preco);
-            return total;
-        }
-        void setItem(double quantidade, Produto produto){
-            Item item = new Item( quantidade,  produto);
-            itens.add(item);
+            Date dataAtual = new Date();
+            List<Mercado.Item> itens;
+            double getTotal() {
+                double total = itens.stream()
+                        .mapToDouble(item -> item.total())
+                        .reduce(0, (acc, preco) -> acc + preco);
+                return total;
+            }
+            void setItem(double quantidade, Produto produto){
+                Item item = new Item( quantidade,  produto);
+                itens.add(item);
+            }
+
+
         }
 
+        public static class Item{
+            double quantidade;
+            Mercado.Produto prod;
+
+
+
+            public Item(double quant, Produto produto) {
+                quantidade = quant;
+                prod = produto;
+            }
+
+            double total(){
+                return quantidade * this.prod.preco;
+            }
+            public double getPreco() {
+                return this.prod.preco;
+            }
+        }
+
+        public static class Produto{
+            double preco;
+            String dataVencimento;
+            String descricao;
+
+            public Produto(double preco, String dataVencimento, String descricao){
+                this.preco = preco;
+                this.dataVencimento = dataVencimento;
+                this.descricao = descricao;
+            }
+
+
+        }
 
     }
 
-    public static class Item{
-        double quantidade;
-        Mercado.Produto prod;
 
-
-
-        public Item() {
-
-        }
-
-        public Item(double quant, Produto produto) {
-            quantidade = quant;
-            prod = produto;
-        }
-
-
-        double total(){
-            return quantidade * this.prod.preco;
-        }
-        public double getPreco() {
-            return this.prod.preco;
-        }
-    }
-
-    public static class Produto{
-        double preco;
-        String dataVencimento;
-        String descricao;
-    }
-
-}
 
