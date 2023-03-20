db1d386994e9004c71f761525885ae3830b6b31c
italo <italogithub@gmail.com>
2023-03-20 11:21:06 -0300

    utilizando o try catch para evitar que o programa quebre na cara do usuario

--- POO-Java/Exercicios/src/AtividadesExtras/Calculadora/AppCalculadora.java
+++ POO-Java/Exercicios/src/AtividadesExtras/Calculadora/AppCalculadora.java
@@ -1,5 +1,6 @@
 package AtividadesExtras.Calculadora;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -10,15 +11,27 @@ public class AppCalculadora {
 
         int opcao = 0;
 
+
         do {
+
+            // utilizei o try catch para evitar que o programa exploda quando o usuario digitar uma letra
             tela();
-            System.out.print("Escolha uma opção: ");
-            opcao = scan.nextInt();
+            boolean tentarNovamente;
+            do {
+                tentarNovamente = false;
+                try {
+                    System.out.print("Escolha uma opção: ");
+                    opcao = scan.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("Opção inválida! Digite um número.");
+                    scan.nextLine(); // limpa o buffer do scanner
+                    tentarNovamente = true;
+                }
+            } while (tentarNovamente);
 
             if (opcao >= 1 && opcao <= 4) {
                 System.out.print("Digite a quantidade de números que deseja calcular: ");
                 int qtdNumeros = scan.nextInt();
-
                 double[] numeros = new double[qtdNumeros];
 
                 for (int i = 0; i < qtdNumeros; i++) {
@@ -27,8 +40,6 @@ public class AppCalculadora {
                 }
 
                 double resultado = 0.0;
-
-
                 switch (opcao) {
                     case 1 -> resultado = Calculadora.soma(numeros);
                     case 2 -> resultado = Calculadora.subtracao(numeros);
@@ -38,15 +49,15 @@ public class AppCalculadora {
 
                 System.out.printf("Resultado: %.2f\n", resultado);
             } else if (opcao != 0) {
-                System.out.println("Opção inválida!");
+                System.out.println("Opção invalida");
             }
         } while (opcao != 0);
+
         scan.close();
     }
 
     private static void tela() {
         Screen tela = new Screen();
-
     }
 }
 
--- POO-Java/Exercicios/src/AtividadesExtras/Calculadora/Screen.java
+++ POO-Java/Exercicios/src/AtividadesExtras/Calculadora/Screen.java
@@ -2,15 +2,16 @@ package AtividadesExtras.Calculadora;
 
 public class Screen {
     {
-        System.out.println(" ============================================== ");
-        System.out.println(" =               Calculadora                  = ");
-        System.out.println(" =                                            = ");
-        System.out.println(" =                1 - Somar                   = ");
-        System.out.println(" =                2 - Subtrair                = ");
-        System.out.println(" =                3 - Dividir                 = ");
-        System.out.println(" =                4 - Multiplicar             = ");
-        System.out.println(" =                0 - Sair                    = ");
-        System.out.println(" =                                            = ");
-        System.out.println(" ============================================== ");
+        System.out.println(" ============================================= ");
+        System.out.println(" =               Calculadora                 = ");
+        System.out.println(" =                                           = ");
+        System.out.println(" =                1 - Somar                  = ");
+        System.out.println(" =                2 - Subtrair               = ");
+        System.out.println(" =                3 - Dividir                = ");
+        System.out.println(" =                4 - Multiplicar            = ");
+        System.out.println(" =                0 - Sair                   = ");
+        System.out.println(" =                                           = ");
+        System.out.println(" =              By: Italo-Mauricio           = ");
+        System.out.println(" ============================================= ");
     }
 }
