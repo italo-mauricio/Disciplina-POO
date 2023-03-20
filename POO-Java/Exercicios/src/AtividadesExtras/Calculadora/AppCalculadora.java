73b17123e4930420b625dcb833d1b82a12851317
italo <italogithub@gmail.com>
2023-03-20 10:22:24 -0300

    add app calculadora: atividades extras POO 1

---
+++ POO-Java/Exercicios/src/AtividadesExtras/Calculadora/AppCalculadora.java
@@ -0,0 +1,57 @@
+package AtividadesExtras.Calculadora;
+
+import java.util.Scanner;
+
+
+
+public class AppCalculadora {
+    public static void main(String[] args) {
+        Screen tela = new Screen();
+
+        Scanner scan = new Scanner(System.in);
+
+        int opcao = 0;
+
+        do {
+            tela();
+            System.out.println("Escolha uma opção: ");
+            opcao = scan.nextInt();
+
+            if (opcao >= 1 && opcao <= 4) {
+                System.out.println("Digite a quantidade de números que deseja calcular:");
+                int qtdNumeros = scan.nextInt();
+
+                double[] numeros = new double[qtdNumeros];
+
+                for (int i = 0; i < qtdNumeros; i++) {
+                    System.out.printf("Digite o %dº número: ", i+1);
+                    numeros[i] = scan.nextDouble();
+                }
+
+                double resultado = 0.0;
+
+
+                switch (opcao) {
+                    case 1 -> resultado = Calculadora.soma(numeros);
+                    case 2 -> resultado = Calculadora.subtracao(numeros);
+                    case 3 -> resultado = Calculadora.divisao(numeros);
+                    case 4 -> resultado = Calculadora.multiplicacao(numeros);
+                }
+
+                System.out.printf("Resultado: %.2f\n", resultado);
+            } else if (opcao != 0) {
+                System.out.println("Opção inválida!");
+            }
+
+        } while (opcao != 0);
+
+        scan.close();
+    }
+
+    private static void tela() {
+    }
+}
+
+
+
+
---
+++ POO-Java/Exercicios/src/AtividadesExtras/Calculadora/Calculadora.java
@@ -0,0 +1,35 @@
+package AtividadesExtras.Calculadora;
+
+class Calculadora{
+    public static double soma(double[] numeros) {
+        double resultado = 0.0;
+        for (double numero : numeros) {
+            resultado += numero;
+        }
+        return resultado;
+    }
+
+    public static double subtracao(double[] numeros) {
+        double resultado = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            resultado -= numeros[i];
+        }
+        return resultado;
+    }
+
+    public static double multiplicacao(double[] numeros) {
+        double resultado = 1.0;
+        for (double numero : numeros) {
+            resultado *= numero;
+        }
+        return resultado;
+    }
+
+    public static double divisao(double[] numeros) {
+        double resultado = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            resultado /= numeros[i];
+        }
+        return resultado;
+    }
+}
