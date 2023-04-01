714b38d9f64579108973ae6584a9b97a2fcd9f0f
italo <italogithub@gmail.com>
2023-04-01 18:47:31 -0300

    utilizando a instanciação simples do objeto

--- POO-Dart/Aulas/EstudoDeClassesEmDart/Pessoa.dart
+++ POO-Dart/Aulas/EstudoDeClassesEmDart/Pessoa.dart
@@ -1,58 +1,52 @@
 import 'dart:io';
 import '../Validacoes/Validacoes.dart';
 
-
-class Pessoa{
+class Pessoa {
   String nome = "";
   int idade = 0;
   String cpf = "";
   double altura = 0;
 
-  void dormir(){
+  void dormir() {
     print("$nome está dormindo!");
   }
 
-  void aniversario(){
+  void aniversario() {
     idade++;
   }
-
 }
-void main(){
 
-  Pessoa pessoa = new Pessoa();  // inicializando a classe
+void main() {
+  Pessoa pessoa = new Pessoa(); // inicializando a classe
 
   do {
-
     // aqui utilizei o do while pois a validação está 100% funcional para STRING
     stdout.write("Digite o seu nome: ");
     pessoa.nome = stdin.readLineSync()!;
-    
-  }while(!validaString(pessoa.nome));
+  } while (!validaString(pessoa.nome));
 
-while (true) {
+  while (true) {
+    // tive que usar uma "gambiarra" pois a validção estava fechando o programa
+    stdout.write("Digite sua idade: ");
+    var entrada = stdin.readLineSync();
+    try {
+      pessoa.idade = int.parse(entrada!);
+      if (validaNumInt(pessoa.idade)) {
+        break;
+      }
+    } catch (e) {}
+    print("Entrada inválida. A idade deve ser um número inteiro.");
+  }
 
-  // tive que usar uma "gambiarra" pois a validção estava fechando o programa
-  stdout.write("Digite sua idade: ");
-  var entrada = stdin.readLineSync();
-  try {
-    pessoa.idade = int.parse(entrada!);
-    if (validaNumInt(pessoa.idade)) {
+  while (true) {
+    stdout.write("Digite o seu cpf: ");
+    var cpf = stdin.readLineSync()!;
+    if (validacpf(cpf)) {
       break;
+    } else {
+      print("CPF Invalido");
     }
-  } catch (e) {
   }
-  print("Entrada inválida. A idade deve ser um número inteiro.");
-}
-
-while(true){
-  stdout.write("Digite o seu cpf: ");
-  var cpf = stdin.readLineSync()!;
-  if(validacpf(cpf)){
-    break;
-  }else{
-    print("CPF Invalido");
-  }
-}
   stdout.write("Digite sua altura: ");
   pessoa.altura = double.parse(stdin.readLineSync()!);
 
@@ -60,6 +54,23 @@ while(true){
   print("Sua idade é: ${pessoa.idade}");
   print("Seu CPF é: ${pessoa.cpf}");
   print("Sua altuera é de: ${pessoa.altura}");
+
+
+
+
+// instanciando de forma simples o segundo objeto
+
+  Pessoa pessoa2 = new Pessoa();
+
+  pessoa2.nome = "Pedro";
+  pessoa2.altura = 1.95;
+  pessoa2.idade = 24;
+  pessoa2.cpf = "2331312321";
+
+  print("Seu nome é: ${pessoa2.nome}");
+  print("Sua idade é: ${pessoa2.idade}");
+  print("Sua altura é: ${pessoa2.altura}");
+  print("Seu CPF é: ${pessoa2.cpf}");
 }
 
 // Ao utilizar o ! após a chamada da função readLineSync(), estamos informando ao compilador que temos certeza de que o valor retornado nunca será nulo. Com isso, o valor é atribuído à variável nome sem problemas.
