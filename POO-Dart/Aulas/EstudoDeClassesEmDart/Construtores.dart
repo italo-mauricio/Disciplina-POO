70f9db027ac0ff60ede3428118c8904353355a64
italo <italogithub@gmail.com>
2023-04-01 20:23:09 -0300

    get e set no dart

--- POO-Dart/Aulas/EstudoDeClassesEmDart/Construtores.dart
+++ POO-Dart/Aulas/EstudoDeClassesEmDart/Construtores.dart
@@ -2,6 +2,15 @@ import 'Pessoa.dart';
 
 // trabalhando com construtores em dart
 
+
+void main() {
+  Carro carro = new Carro("Ferrari", "Spider");
+  Cliente cliente = new Cliente("Italo", "Masculino", "0311234232", 24, 1.85);
+  print(cliente.cpf);
+  print(carro.marca);
+}
+
+
 class Carro {
   String marca = "";
   String modelo = "";
@@ -12,19 +21,32 @@ class Cliente {
   String nome = "";
   String sexo = "";
   String cpf = "";
-  int idade = 0;
-  double altura = 0;
+  int _idade = 0;
+  double _altura = 0;
 
-  Cliente(this.nome, this.sexo, this.cpf, this.idade, this.altura);
+  Cliente(this.nome, this.sexo, this.cpf, this._idade, this._altura);
 
-  Cliente.nascer(this.nome, this.altura) {
-    idade = 0;  // aqui eu crio um construtor com o nome, fazendo um nenem que tem idade 0
+  Cliente.nascer(this.nome, this._altura) {
+    _idade =
+        0; // aqui eu crio um construtor com o nome, fazendo um nenem que tem idade 0
+  }
+
+  // testando os getters e setters no dart
+
+  int get idade => _idade;
+
+  set idade(int idade) {
+    if (idade > 0) {
+      _idade = idade;
+    }
+  }
+
+  double get altura => _altura;
+
+  set altura(double altura) {
+    if (altura > 1.45 && altura <= 3.0) {
+      _altura = altura;
+    }
   }
 }
 
-void main() {
-  Carro carro = new Carro("Ferrari", "Spider");
-  Cliente cliente = new Cliente("Italo", "Masculino", "0311234232", 24, 1.85);
-  print(cliente.cpf);
-  print(carro.marca);
-}
