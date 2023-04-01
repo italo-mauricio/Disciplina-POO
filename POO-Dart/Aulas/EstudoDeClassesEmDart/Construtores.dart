c2d4be8eaf3038c652973b4cacb120c3ccac4fef
italo <italogithub@gmail.com>
2023-04-01 20:07:04 -0300

    construtores com nome

--- POO-Dart/Aulas/EstudoDeClassesEmDart/Construtores.dart
+++ POO-Dart/Aulas/EstudoDeClassesEmDart/Construtores.dart
@@ -2,17 +2,29 @@ import 'Pessoa.dart';
 
 // trabalhando com construtores em dart
 
-class Carro{
+class Carro {
   String marca = "";
-  String modelo = ""; 
+  String modelo = "";
   Carro(this.marca, this.modelo);
 }
 
+class Cliente {
+  String nome = "";
+  String sexo = "";
+  String cpf = "";
+  int idade = 0;
+  double altura = 0;
 
-void main(){
+  Cliente(this.nome, this.sexo, this.cpf, this.idade, this.altura);
 
-  Carro carro = new Carro("Ferrari", "Spider");
-  print(carro.marca);
-
+  Cliente.nascer(this.nome, this.altura) {
+    idade = 0;  // aqui eu crio um construtor com o nome, fazendo um nenem que tem idade 0
+  }
 }
 
+void main() {
+  Carro carro = new Carro("Ferrari", "Spider");
+  Cliente cliente = new Cliente("Italo", "Masculino", "0311234232", 24, 1.85);
+  print(cliente.cpf);
+  print(carro.marca);
+}
